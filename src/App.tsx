40ee205884a863ab8d34ae0cfdc79a9112064976
Karmjit Singh <karmjit.singh@purple.telstra.com>
2021-08-09 10:21:38 +1000

    show resume halt

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React, { useState } from 'react';
 import './App.css';
 import Countdown from "react-countdown";
 
+
 function App() {
     const [completed, setCompleted] = useState(false);
     const [frequency, setFrequency] = useState(0);
     const [numbersMap, setNumbersMap] = useState(new Map());
     const [messages, setMessages] = useState(['Please input the amount of time in seconds between emitting numbers and their frequency']);
     const coundownRef = React.createRef<Countdown>();
+    let [isPaused, setIsPaused] = useState(false);
 
-    const CompletionState = () => <span>{ getMessagesString() } Thank you for playing </span>;
+    const CompletionState = () => <span>{getMessagesString()} Thank you for playing </span>;
 
     const updateMap = (k: any, v: any) => {
         setNumbersMap(new Map(numbersMap.set(k, v)));
@@ -21,20 +23,46 @@ function App() {
             if (value) {
                 value += 1;
                 updateMap(e.target.value, value);
-            } else {
+            } else if (isFibo(e.target.value)) {
+                setMessages([...messages, '>> FIB Please enter the next number']);
+                //setMessages([...messages, '>> Please enter the next number']);
+            }
+            else {
                 value = 1;
                 updateMap(e.target.value, value);
             }
         }
     }
 
+    const isFibo = (number: any) : boolean => {
+
+        const isPerfectSquare = (n: any) => {
+            return n > 0 && Math.sqrt(n) % 1 === 0;
+        }
+
+        if(isPerfectSquare(5*number*number + 4) || isPerfectSquare(5 * number * number - 4)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
     const onStart = () => {
+        if (isPaused) {
+            setMessages([...messages, 'resume']);
+            setIsPaused(false);
+        }
         setCompleted(false);
         coundownRef.current?.getApi().start();
     }
+    const onPause = () => {
+        setMessages([...messages, 'halt']);
+        setIsPaused(true);
+    }
+
     const onQuit = () => {
         coundownRef.current?.getApi().stop();
-
         setCompleted(true);
     }
 
@@ -46,17 +74,15 @@ function App() {
     }
 
     const onTick = () => {
-        console.log(getMessagesString())
-        setMessages([...messages, getMessagesString()]);
-        //setMessages(messages.concat('Enter next number'));
+        setMessages([...messages,' Please enter the next number ' + getMessagesString()]);
     }
 
     const getMessagesString = () => {
         let messageString = '>> ';
-        numbersMap.forEach( (k,v) => {
+        numbersMap.forEach((k, v) => {
             messageString = messageString + `${v}:${k}, `;
-        } );
-        return messageString.substr(0, messageString.length -2);
+        });
+        return messageString.substr(0, messageString.length - 2);
     }
 
     return (
@@ -72,8 +98,8 @@ function App() {
             </div>
             <div className="Actions-box">
                 <input type="button" value="Start" onClick={() => onStart()}/>
-                <input type="button" value="Halt" onClick={() => coundownRef.current?.getApi().pause()}/>
-                <input type="button" value="Resume" onClick={() => coundownRef.current?.getApi().start()}/>
+                <input type="button" value="Halt" onClick={() => onPause()}/>
+                <input type="button" value="Resume" onClick={() => onStart()}/>
                 <input type="button" value="Quit" onClick={() => onQuit()}/>
             </div>
             <div className="Timer-box" hidden={true}>
